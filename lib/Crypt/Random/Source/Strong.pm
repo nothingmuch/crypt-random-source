3a89d8673d0519321519bb4bceb1b17e5a6fc329
Yuval Kogman <nothingmuch@woobling.org>
2008-06-16 23:17:37 +0000

    add Crypt::Random::Source::Strong

---
+++ lib/Crypt/Random/Source/Strong.pm
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+package Crypt::Random::Source::Strong;
+
+use strict;
+use warnings;
+
+sub is_strong { 1 }
+
+__PACKAGE__
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Crypt::Random::Source::Strong - Abstract base class for strong random data
+sources
+
+=head1 SYNOPSIS
+
+	use Squirrel; # or Moose
+
+	extends qw(Crypt::Random::Source::Strong);
+
+=head1 DESCRIPTION
+
+This is an abstract base class. There isn't much to describe.
+
+=head1 METHODS
+
+=over 4
+
+=item is_strong
+
+Returns true
+
+=back
+
+=cut
+
