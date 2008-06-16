c5ed95bce8222801d95055931f679beeb46be477
Yuval Kogman <nothingmuch@woobling.org>
2008-06-16 23:17:54 +0000

    add Crypt::Random::Source::Weak

---
+++ lib/Crypt/Random/Source/Weak.pm
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+package Crypt::Random::Source::Weak;
+use Squirrel;
+
+sub is_strong { 0 }
+
+__PACKAGE__
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Crypt::Random::Source::Weak - Abstract base class for weak random data
+sources
+
+=head1 SYNOPSIS
+
+	use Squirrel; # or Moose
+
+	extends qw(Crypt::Random::Source::Weak);
+
+=head1 DESCRIPTION
+
+This is an abstract base class. There isn't much to describe.
+
+=head1 METHODS
+
+=over 4
+
+=item is_strong
+
+Returns false
+
+=back
+
+=cut
+
