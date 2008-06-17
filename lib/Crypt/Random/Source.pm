b5dff86cd535ed80c115e58478ef952d037b04c2
Yuval Kogman <nothingmuch@woobling.org>
2008-06-17 01:48:27 +0000

    Crypt::Random::Source helper functions

--- lib/Crypt/Random/Source.pm
+++ lib/Crypt/Random/Source.pm
@@ -5,7 +5,28 @@ package Crypt::Random::Source;
 use strict;
 use warnings;
 
+use Sub::Exporter -setup => {
+	exports  => [qw(get get_weak get_strong)],
+	groups => { default => [qw(get get_weak get_strong)] },
+};
+
 our $VERSION = "0.01_01";
+
+use Crypt::Random::Source::Factory;
+
+our ( $factory, $weak, $strong, $any );
+
+sub _factory   ()    { $factory ||= Crypt::Random::Source::Factory->new }
+sub _weak      ()    { $weak    ||= _factory->get_weak }
+sub _strong    ()    { $strong  ||= _factory->get_strong }
+sub _any       ()    { $any     ||= _factory->get }
+
+sub get        ($;@) {    _any->get(@_) }
+sub get_weak   ($;@) {   _weak->get(@_) }
+sub get_strong ($;@) { _strong->get(@_) }
+
+# silence some stupid destructor warnings
+END { undef $weak; undef $strong; undef $any; undef $factory }
 
 __PACKAGE__
 
@@ -19,15 +40,37 @@ Crypt::Random::Source - Get random weak or strong random data.
 
 =head1 SYNOPSIS
 
-	use Crypt::Random::Source;
+	use Crypt::Random::Source qw(get_strong);
+
+	# get 10 cryptographically strong random bytes from an available source
+	my $bytes = get_strong(10);
 
 =head1 DESCRIPTION
 
 This module provides implementations for a number of byte oriented sources of
 random data.
 
-It will also provide a factory with fallback support and an easy api before
-0.01.
+See L<Crypt::Random::Source::Factory> for a more powerful way to locate
+sources, and the various sources for specific implementations.
+
+=head1 EXPORTS
+
+=over 4
+
+=item get
+
+=item get_weak
+
+=item get_strong
+
+These functions delegate to a source chosen by an instance of
+L<Crypt::Random::Source::Factory>, calling get
+
+=back
+
+=head1 SEE ALSO
+
+L<Crypt::Random>, L<Crypt::Util>
 
 =head1 VERSION CONTROL
 
